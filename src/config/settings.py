12c4a62b190204e0bff9923da6132740df9e9362
Ilya Filisteev <ivfilist@mts.ru>
2025-02-07 09:14:04 +0300

    feat: Add DEBUG environment variable and update settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('DJANGO_SECRET_KEY')
 
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('BASE_HOST')]
 
