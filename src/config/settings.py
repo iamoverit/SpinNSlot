ebb46d398869f78a552d1a7d0781ddc36e12f7e7
Ilya Filisteev <ilya.filisteev@gmail.com>
2025-06-10 11:23:32 +0300

    fix bootstrap loading

--- src/config/settings.py
+++ src/config/settings.py
@@ -131,16 +131,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 BOOTSTRAP5 = {
     "css_url": {
-        "url": "https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css",
-        "integrity": "sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT",
-        "crossorigin": "anonymous",
+        "url": f"{STATIC_URL}bootstrap/css/bootstrap.min.css",
     },
 
-    # The complete URL to the Bootstrap bundle JavaScript file.
     "javascript_url": {
-        "url": "https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js",
-        "integrity": "sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO",
-        "crossorigin": "anonymous",
+        "url": f"{STATIC_URL}bootstrap/js/bootstrap.bundle.min.js",
     },
     "color_mode": None,
 }
